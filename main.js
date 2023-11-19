565be209bd623ac884d4a6abbe0fa1ddb65ca536
zlhshn <zsahin4401@gmail.com>
2023-11-19 13:51:09 +0300

    change

--- main.js
+++ main.js
@@ -1,78 +1,103 @@
-const searchBtn = document.getElementById('search')
-const coinList = document.getElementById('coinList')
-const searchInput = document.getElementById('searchInput')
-const form = document.getElementById('form')
+const searchBtn = document.getElementById('search');
+const coinList = document.getElementById('coinList');
+const searchInput = document.getElementById('searchInput');
+const form = document.getElementById('form');
 
-// const nameCoin = document.querySelector('name')
-// const symbol = document.querySelector('symbol')
-// const priceCoin = document.querySelector('price')
-// const img = document.querySelector('img')
-// const change = document.querySelector('change')
-
-
-let allcoins = []
+// let allcoins = []
+// console.log(allcoins);
 
 const getCoin = async () => {
-    const BASE_URL = 'https://api.coinranking.com/v2/coins';
+
+    coinList.innerHTML = '';
+
     try {
-        const res = await fetch(BASE_URL)
-        if (!res.ok) {
-            throw new Error(`Something went wrong:${res.status}`)
+        const BASE_URL = 'https://api.coinranking.com/v2/coins';
+        const res = await fetch(BASE_URL);
+        if (res.ok) {
+
+            const data = await res.json();
+            console.log(data);
+            allcoins = data
+          
+    
+            showCoin(data.data.coins);
+           
+        }else if(!res.ok){
+
+            throw new Error(`Something went wrong:${res.status}`);
         }
 
-        const data = await res.json()
-        allcoins = data
-        // console.log(data);
-        showCoin(data)
-    } catch (error) {
-        console.error(error);
-    }
-}
+      
 
-
-
-const showCoin = (coin) => {
-    console.log(coin.data.coins);
-    
-
-    // coinList.innerHTML = ""; // Clear previous search results
-
-     coin.data.coins.forEach((item) => {
-        const { name, symbol, price, iconUrl, change,color } = item;
-
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-
-            <h2>${name}</h2>
-            <p style= color:${color}>${symbol}</p>
-            <p>${price}</p>
-            <i class="fa-solid fa-chart-line"></i>
-            <div class="svg-icon" style="background-image: url('${iconUrl}')"></div>
-            <p>${change}</p>
-
-        `;
-
-        coinList.appendChild(listItem);
         
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const showCoin = (coins) => {
+
+
+
+  coins.forEach((item) => {
+        const { name, symbol, price, iconUrl, change, color } = item;
+
+        coinList.innerHTML += `
+            <li class="coinClass">
+                <article class='article1'>
+                    <p class="name">${name}</p>
+                    <p class="symbol" style="color: ${color}">${symbol}</p>
+                </article>
+                <p class="price">$ ${price}</p>
+                <div class="img svg-icon" style="background-image: url('${iconUrl}')"></div>
+                <article class='change'><i class="fa-solid fa-chart-line"></i>
+                    <p class="change">${change}</p>
+                </article>
+            </li>`;
+
+        const div = document.querySelector('.change');
+
+        if (change) {
+            change >= 0 ? div.classList.add('green') : div.classList.add('red');
+        }
     });
 
    
-}
+};
 
 
 
-// searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener('click', ()=>{
+
+    const value = searchInput.value.toLowerCase().trim();
+    console.log(value);
+
+    if (value) {
+        // const filteredCoins = allcoins.filter((coin) =>
+        //     coin.name.toLowerCase().includes(value) || coin.symbol.toLowerCase().includes(value)
+        // );
+        showCoin();
+    } else {
+        alert('Lütfen bir kullanıcı ismi giriniz.');
+    }
+});
+
+// const search = () => {
 //     const value = searchInput.value.toLowerCase().trim();
 //     console.log(value);
 
 //     if (value) {
-//         const filteredCoins = allcoins.data.coins.filter(coin =>
-//             coin.name.toLowerCase().includes(value) || coin.symbol.toLowerCase().includes(value)
-//         );
-//         showCoin(filteredCoins);
+//         // const filteredCoins = allcoins.filter((coin) =>
+//         //     coin.name.toLowerCase().includes(value) || coin.symbol.toLowerCase().includes(value)
+//         // );
+//         showCoin();
 //     } else {
-//         alert("Lütfen bir kullanıcı ismi giriniz.");
+//         alert('Lütfen bir kullanıcı ismi giriniz.');
 //     }
-// });
+// };
 
-// getCoin();
+
+
+
+
