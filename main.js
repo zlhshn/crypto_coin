c508654286021eb4ebe4e817d4e83674f0e6b236
zlhshn <zsahin4401@gmail.com>
2023-11-20 17:35:45 +0300

    style

--- main.js
+++ main.js
@@ -3,51 +3,60 @@ const coinList = document.getElementById('coinList');
 const searchInput = document.getElementById('searchInput');
 const form = document.getElementById('form');
 
-// let allcoins = []
-// console.log(allcoins);
+
 
 
 const getCoin = async () => {
 
-    coinList.innerHTML = '';
-    const BASE_URL = 'https://api.coinranking.com/v2/coins';
-        const res = await fetch(BASE_URL);
+    coinList.innerHTML  =''
 
+   
     try {
-        
-        if (res.ok) {
-            const BASE_URL = 'https://api.coinranking.com/v2/coins';
+        const BASE_URL = 'https://api.coinranking.com/v2/coins';
         const res = await fetch(BASE_URL);
+        const veri = await res.json();
+        if (res.ok) {
+    console.log(res);
 
-            const veri = await res.json();
-            console.log(veri);
-            allcoins = veri.data.coins
-            console.log(allcoins);
-            showCoin(veri)
-          
+        
+
+         console.log(veri);
+         const coins = veri.data.coins
+         allcoins = coins
+         console.log(allcoins);
+        //  showCoin(coins)
+    
          
         }else if(!res.ok){
             throw new Error(`Something went wrong:${res.status}`);
         }
+
+        
     } catch (error) {
         console.log(error);
     }
+
+
 };
-
-
 
 getCoin()
 
+let allcoins = []
+
+
 const showCoin = (coin) => {
 
-  console.log(coin);
- 
+  
+ coin.forEach((item)=>{
 
-    const { name, symbol, price, iconUrl, change, color } = coin
+    const { name, symbol, price, iconUrl, change, color } = item
 
 
-    coinList.innerHTML += `
+
+    if (!coinList.innerHTML.includes(name)){
+         coinList.innerHTML += `
         <li class="coinClass">
+        <i class="fa-solid fa-x"></i>
             <article class='article1'>
                 <p class="name">${name}</p>
                 <p class="symbol" style="color: ${color}">${symbol}</p>
@@ -58,41 +67,81 @@ const showCoin = (coin) => {
                 <p class="change">${change}</p>
             </article>
         </li>`;
-
-    const div = document.querySelector('.change');
-
-    if (change) {
-        change >= 0 ? div.classList.add('green') : div.classList.add('red');
     }
+   
+
+        const classChange = document.querySelectorAll(".change")
+
+    
+        classChange.forEach((n)=>{
+            if(n.textContent >= 0){n.classList.add("green")
+        }else{
+        n.classList.add("red")
+        }
+ })
+
+
 
  
-       
+})
 
-};
+}
 
 
-window.addEventListener('click',()=>{
 
+window.addEventListener('load',()=>{
+
+    searchInput.innerText =''
     getCoin()
 })
 
 
 
-// searchBtn.addEventListener('click',(e)=>{
-//     e.preventDefault()
+form.addEventListener('submit',(e)=>{
+    // coinList.innerHTML  =''
+    e.preventDefault()
 
-//     const searchTerm = searchInput.value.trim().toLowerCase();
+    if(!searchInput.value){
 
-//     if (searchTerm) {
-//         // Filter coins based on the exact match of the name
-//         const filteredCoins = allcoins.filter(coin => coin.name.toLowerCase() == searchTerm);
-//         console.log(filteredCoins);
-//        showCoin(filteredCoins)
-//        searchInput.innerText =''
-//     } else {
-//         alert('Please enter a value.');
-//     }
+        alert('please enter a coin name')
+    } else{
+ const value = searchInput.value
+
+        const filteredCoins = allcoins.filter(coin => coin.name.toLowerCase().includes(value.toLowerCase()));
+        console.log(filteredCoins);
+       showCoin(filteredCoins)
+
+    }
+
+   
+
+       searchInput.value =''
+  
+
+})
+
+
+
+const deleteİcon = document.querySelector('.fa-x')
+
+coinList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-x')) {
+        e.target.closest('.coinClass').remove();
+    }
+});
+
+
+
+
+
+// searchBtn.addEventListener('click',()=>{
+
+//     const value = searchInput.value
+
+//     const filteredCoins = allcoins.filter(coin => coin.name.toLowerCase().includes(value.toLowerCase()));
+//     console.log(filteredCoins);
+//    showCoin(filteredCoins)
+
+//    searchInput.innerText =''
 
 // })
-
-
